04f2731b4ad6233010fbec1fa8ff97123d13b156
Josiel Faleiros <josiel.2015@alunos.utfpr.edu.br>
2016-09-24 01:08:04 -0300

    gui

--- src/exercicio_06/Teste.java
+++ src/exercicio_06/Teste.java
@@ -4,8 +4,23 @@ package exercicio_06;
  * @author Joao Paulo Bernardino Maciel
  * @author Josiel Faleiros Alves
  */
-public static class Teste(){
+public class Teste{
     public static void main(String[] args) {
-        //TODO
+        System.out.println(
+            "\n\t+---------------------------------------------------------------+" +
+            "\n\t|\tSistema de Gestão de Veículos - Menu Inicial            |" +
+            "\n\t+---------------------------------------------------------------+" +
+            "\n\t|\t1)_ Cadastrar Veículo de Passeio                        |" +
+            "\n\t|\t2)_ Cadastrar Veículo de Carga                          |" +
+            "\n\t|\t3)_ Imprimir Todos os Veículos de Passeio               |" +
+            "\n\t|\t4)_ Imprimir Todos os Veículos de Carga                 |" +
+            "\n\t|\t5)_ Imprimir Veículo de Passeio pela Placa              |" +
+            "\n\t|\t6)_ Imprimir Veículo de Carga pela Placa                |" +
+            "\n\t|\t7)_ Alterar dados do Veículo de Passeio pela Placa      |" +
+            "\n\t|\t8)_ Alterar dados do Veículo de Carga pela Placa        |" +
+            "\n\t|\t9)_ Sair do Sistema                                     |" +
+            "\n\t|                                                               |" +
+            "\n\t|                                                               |" +
+            "\n\t|\tEscolha uma opção:                                      |");
     }
 }
