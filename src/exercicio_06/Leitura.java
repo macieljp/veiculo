d780778c70ecf0fefc0aa436a2afda2bae5ff267
Josiel Faleiros <josiel.2015@alunos.utfpr.edu.br>
2016-09-25 16:27:46 -0300

    classe Leitura implementada com sucesso

--- src/exercicio_06/Leitura.java
+++ src/exercicio_06/Leitura.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio_06;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  *
@@ -12,8 +11,23 @@ package exercicio_06;
  */
 public class Leitura {
 
+    /*
+    - receberá uma String @entrada como parâmetro: esta será impressa na tela como informação ao usuário de que
+        dado do veiculo ele está informando ao sistema;
+    - retornará uma String: valor informado pelo usuário para aquele dado.
+    */
     public String inDados(String entrada){
-        
+        System.out.println(entrada);
+
+        String info = "";
+        BufferedReader memoria = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            info = memoria.readLine();
+        }
+        catch(IOException objIO){
+            System.out.println("\n erro de sistema");
+        }
+        return info;
     }
 
 }
