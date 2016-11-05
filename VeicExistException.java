0a044ce74dc5223a9650b3f8acbc95bec453c84c
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2016-11-04 23:36:54 -0200

    [Exception] classes Exception criadas 	new file:   VeicExistException.java 	new file:   VelocException.java

---
+++ VeicExistException.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Joao Paulo Bernardino Maciel
+ * @author Josiel Faleiros Alves
+ */
+public class VeicExistException extends Exception {
+    public VeicExistException(){
+        System.out.println("\n Exceção lançada por VeicExistException");
+    }
+}
---
+++ VelocException.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Joao Paulo Bernardino Maciel
+ * @author Josiel Faleiros Alves
+ */
+public class VelocException extends Exception {
+	public VelocException(){
+		System.out.println("\n Exceção lançada por VelocException");
+	}
+}
